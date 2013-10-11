6d09b620dbfc630d7a4d530ea674414ebd3d0b8c
shefun <shefun@bk.ru>
2013-10-11 22:56:47 +0300

    save

--- src/common/directives/map.js
+++ src/common/directives/map.js
@@ -100,11 +100,46 @@ angular.module('directives.gmap', ['services.connect', 'services.eventmarker', '
           }, 250);
         }
         animateCircle();
+        
+        scope.points = [];
+        
+        var updatePoints = function(points) {
+            scope.points = points;
+        }
+        scope.distance = function (p1, p2) {
+             var R = 6371; // km (change this constant to get miles)
+             var dLat = (p2.lat-p1.lat) * Math.PI / 180;
+             var dLon = (p2.lon-p1.lon) * Math.PI / 180;
+             var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+              Math.cos(p1.lat * Math.PI / 180 ) * Math.cos(p2.lat * Math.PI / 180 ) *
+              Math.sin(dLon/2) * Math.sin(dLon/2);
+             var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+             var d = R * c;
+             return d;
+          }
+        scope.findNearestPoint = function(M) {
+                if (scope.points.length == 0)
+                    return null;
+                var points = scope.points;
+                var point = null;
+                var minDistance = 1000000000;
+                //console.log(scope);
+                for (var i = 0; i < points.length; ++i) {
+                    var distance = scope.distance(points[i], M);
+                    if (minDistance > distance) {
+                        point = points[i];
+                        minDistance = distance;
+                    }
+                }
+                return point;
+            }
+        scope.infowindow = null;
 
         var showTrack = function(data){
 
             // console.log("showTrack", data);
-
+            updatePoints (data.points);
+            console.log("ShowTrack");
             path = new google.maps.Polyline({
                 path: data.track,
                 strokeColor: 'blue',
@@ -124,10 +159,20 @@ angular.module('directives.gmap', ['services.connect', 'services.eventmarker', '
                     }],
                 map: map
             });
+            
 
-            google.maps.event.addListener(path, 'click', function(event )
+            google.maps.event.addListener(path, 'click', function(event)
             {
-                console.log(event);
+                //console.log(event);
+                var point = scope.findNearestPoint({lat:event.latLng.lb, lon:event.latLng.mb});
+                console.log(point);
+                if (scope.infowindow != null)
+                    scope.infowindow.close();
+                scope.infowindow = new google.maps.InfoWindow({
+                    content: "egethryh",
+                    position: new google.maps.LatLng(point.lat, point.long)
+                });
+                //scope.infowindow.open(map);
             });
 
 
@@ -196,7 +241,7 @@ angular.module('directives.gmap', ['services.connect', 'services.eventmarker', '
                 path = null;
                 eventmarker.setData([]);
             }
-            if((data === null) || (data.points.length === 0) ) return;
+            if((data === null) || (data.points.length === 0) ) return; 
             showTrack(data);
         }, true);
         // scope.$watch("track.select", function(data){
@@ -204,50 +249,6 @@ angular.module('directives.gmap', ['services.connect', 'services.eventmarker', '
         // });
 
         var lastmarker = new LastMarker(map);
-        /*//TODO убрать это тестовые данные!!!
-        ///////////////////////////////
-        var lastpos = [{
-            dynamic: {
-                course: 64,
-                csq: "22",
-                dt: 1380475196,
-                flags: "0",
-                fsource: 3,
-                fuel: 0,
-                lastping: 1380475230,
-                latitude: 48.509835,
-                longitude: 34.58397166666666,
-                sats: 8,
-                speed: 0,
-                vin: 4.2,
-                vout: 2.39
-            },
-            icon: "caricon-truck",
-            key: "MDEzMjI2MDAyNDM2NDIb",
-            title: "HYUNDAI"
-        },
-                      {
-            dynamic: {
-                course: 64,
-                csq: "22",
-                dt: 1380475196,
-                flags: "0",
-                fsource: 3,
-                fuel: 0,
-                lastping: 1380475230,
-                latitude: 48.809835,
-                longitude: 34.58397166666666,
-                sats: 8,
-                speed: 80,
-                vin: 4.2,
-                vout: 10.39
-            },
-            icon: "caricon-truck",
-            key: "MDEzMjI2MDAyNDM2NDIy",
-            title: "test2"
-        }];
-        lastmarker.setData(lastpos);
-        ///////////////////////////////*/
 
         scope.$watch("systems", function(systems){
             if(!systems) return;
@@ -294,3 +295,8 @@ angular.module('directives.gmap', ['services.connect', 'services.eventmarker', '
         }]*/
     };
 }]);
+
+/*
+<div style="cursor: default; width: 264px; height: 178px; position: absolute; left: 576px; top: 284px; z-index: 284;"><div style="position: absolute; left: 0px; top: 0px;"><div style="width: 0px; height: 0px; border-right-width: 10px; border-right-style: solid; border-right-color: transparent; border-left-width: 10px; border-left-style: solid; border-left-color: transparent; border-top-width: 24px; border-top-style: solid; border-top-color: rgba(0, 0, 0, 0.0980392); position: absolute; left: 122px; top: 178px;"></div><div style="position: absolute; left: 0px; top: 0px; background-color: rgba(0, 0, 0, 0.2); border-top-left-radius: 2px; border-top-right-radius: 2px; border-bottom-right-radius: 2px; border-bottom-left-radius: 2px; -webkit-box-shadow: rgba(0, 0, 0, 0.298039) 0px 1px 4px -1px; box-shadow: rgba(0, 0, 0, 0.298039) 0px 1px 4px -1px; width: 264px; height: 178px;"></div><div style="border-top-width: 24px; position: absolute; left: 122px; top: 175px;"><div style="position: absolute; overflow: hidden; left: -6px; top: -1px; width: 16px; height: 30px;"><div style="position: absolute; left: 6px; background-color: rgb(255, 255, 255); -webkit-transform: skewX(22.6deg); -webkit-transform-origin: 0px 0px; height: 24px; width: 10px; box-shadow: rgba(0, 0, 0, 0.6) 0px 1px 6px;"></div></div><div style="position: absolute; overflow: hidden; top: -1px; left: 10px; width: 16px; height: 30px;"><div style="position: absolute; left: 0px; background-color: rgb(255, 255, 255); -webkit-transform: skewX(-22.6deg); -webkit-transform-origin: 10px 0px; height: 24px; width: 10px; box-shadow: rgba(0, 0, 0, 0.6) 0px 1px 6px;"></div></div></div><div style="position: absolute; left: 1px; top: 1px; border-top-left-radius: 2px; border-top-right-radius: 2px; border-bottom-right-radius: 2px; border-bottom-left-radius: 2px; background-color: rgb(255, 255, 255); width: 262px; height: 176px;"></div></div><div class="gm-style-iw" style="position: absolute; left: 12px; top: 9px; overflow: auto; width: 220px; height: 160px;"><div class="" style="overflow: auto;"><div style="width: 220px; height: 160px; border: none;"><div class="info-header">11/10/2013 8:19:36</div><table id="tbl_info" width="100%"><tbody><tr><td>Направление:</td><td><b>245°</b></td></tr><tr><td>Долгота:</td><td><b>48.40672</b></td></tr><tr><td>Широта:</td><td><b>35.04784</b></td></tr><tr><td>Спутники</td><td><b>8</b></td></tr><tr><td>Скорость</td><td><b>36.6км/ч</b></td></tr><tr><td>Основное питание</td><td><b>13.8В</b></td></tr><tr><td>Резервное питание</td><td><b>4.21В</b></td></tr><tr><td>Тип метки</td><td><b>ANGLE</b></td></tr></tbody></table><div id="moreinfo" title="Ожидайте, идет получение дополнительной информации."></div></div></div></div><div style="width: 13px; height: 13px; overflow: hidden; position: absolute; opacity: 0.7; right: 12px; top: 12px; z-index: 10000; cursor: pointer;"><img src="http://maps.gstatic.com/mapfiles/api-3/images/mapcnt3.png" draggable="false" style="position: absolute; left: -2px; top: -336px; width: 59px; height: 492px; -webkit-user-select: none; border: 0px; padding: 0px; margin: 0px;"></div></div>
+
+*/
