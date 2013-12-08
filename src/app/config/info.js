1dedfe846a159a19c96e20fbe9ae4e15841217c0
shefun <shefun@bk.ru>
2013-12-09 01:56:49 +0200

    save

--- src/app/config/info.js
+++ src/app/config/info.js
@@ -86,7 +86,7 @@ angular.module('config.system.info', ['ngRoute', '$strap', 'resources.params', '
             return result;
         };
 
-        $scope.caricons = ('caricon-android-1 caricon-reddit caricon-plancast caricon-angellist caricon-home caricon-cloud caricon-umbrella caricon-star ' +
+        $scope.caricons = ('caricon-reddit caricon-plancast caricon-angellist caricon-home caricon-cloud caricon-umbrella caricon-star ' +
             'caricon-star-empty caricon-attention caricon-flight caricon-flight-1 caricon-accessibility caricon-paper-plane-1 ' +
             'caricon-money caricon-beaker caricon-truck-1 caricon-attach caricon-guidedog caricon-lightbulb caricon-blind ' +
             'caricon-basket caricon-paper-plane caricon-traffic-cone caricon-cc caricon-emo-happy caricon-aboveground-rail caricon-airfield ' +
@@ -95,11 +95,14 @@ angular.module('config.system.info', ['ngRoute', '$strap', 'resources.params', '
             'caricon-swimming caricon-crown caricon-twitter caricon-user-md caricon-ambulance caricon-fighter-jet caricon-h-sigh ' +
             'caricon-github caricon-shield caricon-extinguisher caricon-rocket caricon-anchor caricon-apple caricon-android ' +
             'caricon-linux caricon-female caricon-male caricon-bug caricon-twitter-1 caricon-evernote caricon-globe caricon-globe-alt ' +
-            'caricon-award caricon-rocket-1 caricon-truck').split(' ').map(function(i) {
+            'caricon-award caricon-rocket-1 caricon-truck' //+
+            //' caricon_set_2-truck caricon_set_2-icon_765 caricon_set_2-icon_1609 caricon_set_2-icon_15944 caricon_set_2-icon_8622 caricon_set_2-icon_609 caricon_set_2-icon_12525 caricon_set_2-icon_23424 caricon_set_2-icon_16784 caricon_set_2-icon_22821 caricon_set_2-icon_13135 caricon_set_2-icon_11868 caricon_set_2-icon_7504 caricon_set_2-icon_1892 caricon_set_2-icon_12844 caricon_set_2-icon_12241 caricon_set_2-icon_17587 caricon_set_2-icon_23115 caricon_set_2-icon_2558 caricon_set_2-icon_26574 caricon_set_2-icon_19523 caricon_set_2-icon_2690 caricon_set_2-icon_14692 caricon_set_2-icon_1340 caricon_set_2-icon_4506 caricon_set_2-icon_18560 caricon_set_2-icon_11427 caricon_set_2-icon_22186 caricon_set_2-icon_7883 caricon_set_2-icon_18512 caricon_set_2-icon_20591 caricon_set_2-icon_118682 caricon_set_2-icon_17054 caricon_set_2-icon_18540 caricon_set_2-noun_project_22186 caricon_set_2-noun_project_10661 caricon_set_2-icon_9258 caricon_set_2-icon_7727 caricon_set_2-icon_24929 caricon_set_2-icon_26574 caricon_set_2-icon_10144 caricon_set_2-icon_13082 caricon_set_2-icon_1360 caricon_set_2-icon_17187 caricon_set_2-icon_21007 caricon_set_2-icon_22277 caricon_set_2-icon_19359 caricon_set_2-icon_17004 caricon_set_2-icon_1514 caricon_set_2-icon_23560 caricon_set_2-icon_4699 caricon_set_2-icon_5375'
+                          ).split(' ').map(function(i) {
             return {
                 class: i
             };
         });
+        
 
         $scope.changeIcon = function() {
             var options = {};
